2e0208da274090b2f86a40f94ed6399d29f4cd6d
Sei Fukushima <sftakkyu1283@gmail.com>
2023-02-05 00:38:23 +0900

    feat: CORS本番設定;2C;2C

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,16 +1,13 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
-
+    origins [
+      'http://localhost:3001',
+      'https://ku-lab-match.vercel.app',
+      'https://ku-lab-match-git-*-replica531.vercel.app'
+    ]
     resource '*',
+             methods: %i[get post put patch delete options head],
              headers: :any,
-             methods: %i[get post put patch delete options head], expose: ['access-token']
+             credentials: true
   end
 end
